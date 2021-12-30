0daed552367bb75b5381344dd439366a70650ddf
Cooper Saye <coopersaye@gmail.com>
2021-12-29 23:40:22 -0800

    Implement google auth.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
+import signInWithGoogle from '../util/signInWithGoogle';
+import { getAuth, signOut } from 'firebase/auth';
+
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
+  const auth = getAuth();
+
   return (
     <div>
+      {
+        auth.currentUser ?
+        <button onClick={() => signOut(auth)}>
+          Sign Out
+        </button> :
+        <button onClick={signInWithGoogle}>
+          Sign in with Google
+        </button>
+      }
     </div>
   );
 }
