921599a0dcabb778e0fe2c3768e287f4abbab269
Cooper Saye <coopersaye@gmail.com>
2021-12-29 22:54:45 -0800

    Import firebase config.

---
+++ util/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDBr1_l0D55W99-s858m1EpaC0o6pgQS3U",
+  authDomain: "fields-fun.firebaseapp.com",
+  projectId: "fields-fun",
+  storageBucket: "fields-fun.appspot.com",
+  messagingSenderId: "14056801392",
+  appId: "1:14056801392:web:569d79b85ad408658020b5"
+};
