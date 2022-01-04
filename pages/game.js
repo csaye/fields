d43fba20624c8cc002e528c3d0b11105b252c7a0
Cooper Saye <coopersaye@gmail.com>
2022-01-03 23:41:44 -0800

    Create game canvas.

--- pages/game.js
+++ pages/game.js
@@ -1,9 +1,23 @@
+import { useEffect, useRef } from 'react';
+
 import styles from '../styles/pages/Game.module.css';
 
+let canvas;
+let ctx;
+
 export default function Game() {
+  const canvasRef = useRef();
+
+  // get canvas on start
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
+
   return (
     <div>
       <h1>Game</h1>
+      <canvas ref={canvasRef} />
     </div>
   );
 }
