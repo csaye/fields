b5fd95bf9b1b4134ec3cc76a9f9bf73e44c9518b
Cooper Saye <coopersaye@gmail.com>
2022-01-10 08:10:46 -0800

    Implement canvas resize.

--- pages/game.js
+++ pages/game.js
@@ -11,6 +11,12 @@ export default function Game() {
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
 
+  // called on window resize
+  function onResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+
   // get canvas on start
   useEffect(() => {
     canvas = canvasRef.current;
@@ -18,6 +24,9 @@ export default function Game() {
     // initialize dimensions
     setWidth(window.innerWidth);
     setHeight(window.innerHeight);
+    // set up resize listener
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
   }, []);
 
   return (
