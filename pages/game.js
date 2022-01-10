93a6f22732ad5d84504ce01edfb7cd21d0fc3ee1
Cooper Saye <coopersaye@gmail.com>
2022-01-10 08:10:36 -0800

    Initialize canvas dimensions.

--- pages/game.js
+++ pages/game.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import styles from '../styles/pages/Game.module.css';
 
@@ -8,16 +8,25 @@ let ctx;
 export default function Game() {
   const canvasRef = useRef();
 
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
   // get canvas on start
   useEffect(() => {
     canvas = canvasRef.current;
     ctx = canvas.getContext('2d');
+    // initialize dimensions
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
   }, []);
 
   return (
     <div>
-      <h1>Game</h1>
-      <canvas ref={canvasRef} />
+      <canvas
+        ref={canvasRef}
+        width={width}
+        height={height}
+      />
     </div>
   );
 }
