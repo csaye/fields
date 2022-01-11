fbe1c7d9d2ea351e03c8fb7a20d793e119f57a79
Cooper Saye <coopersaye@gmail.com>
2022-01-10 23:20:43 -0800

    Add header elements.

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,30 @@
 import Link from 'next/link';
 
+import signInWithGoogle from '../util/signInWithGoogle';
+import { getAuth, signOut } from 'firebase/auth';
+
 import styles from '../styles/components/Header.module.css';
 
 export default function Header() {
+  const auth = getAuth();
+
   return (
     <div className={styles.container}>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
       <Link href="/game">
         <a>Game</a>
       </Link>
+      {
+        auth.currentUser ?
+        <button onClick={() => signOut(auth)}>
+          Sign Out
+        </button> :
+        <button onClick={signInWithGoogle}>
+          Sign in with Google
+        </button>
+      }
     </div>
   );
 }
