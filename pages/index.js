ffa30b6d16f712a9e168c8d47e6a34e9defd8cb5
Cooper Saye <coopersaye@gmail.com>
2022-01-10 23:20:37 -0800

    Clean up index page.

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,12 @@
 import Header from '../components/Header';
 
-import signInWithGoogle from '../util/signInWithGoogle';
-import { getAuth, signOut } from 'firebase/auth';
-
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
-  const auth = getAuth();
-
   return (
     <div>
       <Header />
-      {
-        auth.currentUser ?
-        <button onClick={() => signOut(auth)}>
-          Sign Out
-        </button> :
-        <button onClick={signInWithGoogle}>
-          Sign in with Google
-        </button>
-      }
+      <h1>Fields</h1>
     </div>
   );
 }
