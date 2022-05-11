80b2ad1446b59ff54c45205d99b7e11db573acb0
Cooper Saye <coopersaye@gmail.com>
2022-05-10 20:23:35 -0700

    Call create player on sign in.

--- util/signInWithGoogle.js
+++ util/signInWithGoogle.js
@@ -4,9 +4,13 @@ import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 const auth = getAuth();
 const db = getFirestore();
 
+// creates player doc if none existing
+async function createPlayer() {
+}
+
 // opens google sign in popup
 export default async function signInWithGoogle() {
-  const auth = getAuth();
   const provider = new GoogleAuthProvider();
   await signInWithPopup(auth, provider);
+  createPlayer();
 }
