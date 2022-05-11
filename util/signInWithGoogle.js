17fecb0099503bbec87e33cef5b24b26aa64e197
Cooper Saye <coopersaye@gmail.com>
2022-05-10 20:23:35 -0700

    Import firestore.

--- util/signInWithGoogle.js
+++ util/signInWithGoogle.js
@@ -1,4 +1,8 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const auth = getAuth();
+const db = getFirestore();
 
 // opens google sign in popup
 export default async function signInWithGoogle() {
