e7b29b09a37d5a97e887b898536dabc1b89a81ee
Cooper Saye <coopersaye@gmail.com>
2022-05-11 21:34:53 -0700

    Remove old states.

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,8 @@ import signInWithGoogle from '../util/signInWithGoogle';
 
 import styles from '../styles/pages/Index.module.css';
 
-
-export default function Index() {
-  const auth = getAuth();
-  const db = getFirestore();
-
-  const canvasRef = useRef();
-
-  const uid = auth.currentUser.uid;
-  const userRef = doc(db, 'players', uid);
-
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
-
-  useEffect(() => {
-    draw();
-  }, [width, height]);
+export default function Index(props) {
+  const { authed } = props;
 
   return (
     <div className={styles.container}>
