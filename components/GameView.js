de8e11ba5ec1773535975760b1039dab7b7bcb76
Cooper Saye <coopersaye@gmail.com>
2022-05-12 19:12:37 -0700

    Import from firebase.

--- components/GameView.js
+++ components/GameView.js
@@ -1,5 +1,11 @@
 import styles from '../styles/components/GameView.module.css';
 
+import { useEffect, useRef, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import {
+  getFirestore, increment, collection, doc, updateDoc
+} from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 export default function GameView() {
   return (
     <div>
