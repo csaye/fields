ec5cf33ad8af44336bf29e5ad7b8daf10a97a267
Cooper Saye <coopersaye@gmail.com>
2022-05-12 19:12:37 -0700

    Set up map dimensions.

--- components/GameView.js
+++ components/GameView.js
@@ -6,6 +6,14 @@ import {
   getFirestore, increment, collection, doc, updateDoc
 } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const mapSize = 15;
+const midMap = Math.floor(mapSize / 2);
+const border = 1;
+
+let canvas;
+let ctx;
+
 export default function GameView() {
   return (
     <div>
